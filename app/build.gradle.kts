cb46de45a1870e8c146c50e6a8ac26de374b8460
WilsonAlmonte <wilson.a.almonte31@gmail.com>
2024-05-19 16:46:06 -0400

    chore: installs dagger hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -50,7 +52,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,7 +67,18 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.viewmodel.compose)
+
+//    dependency injection
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+//    firebase dependencies
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
-    implementation(libs.viewmodel.compose)
+    implementation(libs.firebase.auth)
+}
+
+kapt {
+    correctErrorTypes = true
 }
