15418059ff2bc1fdc43eceda9e76fb2b7388738d
WilsonAlmonte <wilson.a.almonte31@gmail.com>
2024-05-23 12:24:56 -0400

    feat: creates project Button TextField base atoms

---
+++ app/src/main/java/com/codemote/puppytimev2/ui/atoms/PuppyButton.kt
@@ -0,0 +1,36 @@
+package com.codemote.puppytimev2.ui.atoms
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun PuppyButton(buttonText: String, onClick: () -> Unit) {
+    ElevatedButton(
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(),
+        shape = MaterialTheme.shapes.medium,
+        contentPadding = PaddingValues(16.dp),
+    ) {
+        Text(
+            text = buttonText,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun PuppyPrimaryButton(buttonText: String, onClick: () -> Unit) {
+    PuppyButton(buttonText = buttonText, onClick = { onClick() })
+}
